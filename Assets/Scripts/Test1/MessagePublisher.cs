ea303c317c2fcf0dba248957bb0884dd739c2cb6
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-23 06:07:15 +0900

    メッセージ送信側と受信側のクラスをそれぞれとりあえず定義

---
+++ Assets/Scripts/Test1/MessagePublisher.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test1
+{
+    /// <summary>
+    /// メッセージ送信側
+    /// </summary>
+    public class MessagePublisher : MonoBehaviour
+    {
+    }
+}
---
+++ Assets/Scripts/Test1/MessageReceiver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test1
+{
+    /// <summary>
+    /// メッセージ受信側
+    /// </summary>
+    public class MessageReceiver : MonoBehaviour
+    {
+    }
+}
