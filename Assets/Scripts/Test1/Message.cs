da688db15434251c99d9c4b992b369f4648d3a37
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-23 06:15:50 +0900

    MessagePipeを使ってやり取りするクラスを定義

---
+++ Assets/Scripts/Test1/Message.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test1
+{
+    /// <summary>
+    /// メッセージ
+    /// </summary>
+    public class Message
+    {
+        /// <summary>
+        /// 送信メッセージ
+        /// </summary>
+        public string SendMessage { get; private set; } = "";
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="SendMessage">送信メッセージ</param>
+        public Message(string SendMessage)
+        {
+            this.SendMessage = SendMessage;
+        }
+    }
+}
